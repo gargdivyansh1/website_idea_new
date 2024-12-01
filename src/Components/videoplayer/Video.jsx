615e3f4c3ab571c8439c4bf07cebd3866ad3c141
gargdivyansh1 <divyanshgarg515@gmail.com>
2024-12-01 09:26:20 +0530

    adding responsiveness to the site

---
+++ src/Components/videoplayer/Video.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Video.css'
+import video from '../../assets/college-video.mp4'
+
+const Video = () => {
+  return (
+    <div className='video-player'>
+        {/* <video src={}></video> */}
+    </div>
+  )
+}
+
+export default Video
